4759d4c01399bd264eb93609de38edbad92a1af2
Yiyun Li <57505569+liyiyunnn@users.noreply.github.com>
2023-10-16 08:43:38 +0200

    Delete annotation_network.R

--- annotation_network.R
+++
@@ -1,9 +0,0 @@
-con <- t(read.table("/Users/liyiyun/Library/CloudStorage/OneDrive-WageningenUniversity&Research/VICI-ENW projects/Data analysis/GRN/input_94/expset_con_alllayer.txt",sep = "\t"))
-salt <-t(read.table("/Users/liyiyun/Library/CloudStorage/OneDrive-WageningenUniversity&Research/VICI-ENW projects/Data analysis/GRN/input_94/expset_salt_alllayer.txt",sep = "\t"))
-rownames(con) == rownames(salt)
-input = cbind(con,salt)
-write.table(input, "/Users/liyiyun/Library/CloudStorage/OneDrive-WageningenUniversity&Research/VICI-ENW projects/Data analysis/GRN/input_94/input_exp.txt",sep = "\t",quote = F)
-
-exp <- read.table("/Users/liyiyun/Desktop/network-grn/expset_salt.txt",sep = "\t")
-exp.in <- exp[rownames(con),]
-write.table(exp.in, "/Users/liyiyun/Library/CloudStorage/OneDrive-WageningenUniversity&Research/VICI-ENW projects/Data analysis/GRN/input_94/input_94_expression.txt",sep = "\t",quote = F)
